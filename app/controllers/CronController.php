8a17218082dfa8110644b4fcb5c25affc5c472d5
AndrewKanaew <andrewrook-media@yandex.ru>
2018-10-23 17:34:16 +0300

    Реализовано добавление заказов из TopDelivery

--- app/controllers/CronController.php
+++ app/controllers/CronController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 
+use App\Models\Order;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -16,18 +17,44 @@ class CronController extends Controller
     /**
      * @param RequestInterface $request
      * @param ResponseInterface $response
+     * @return mixed
      */
 
     public function index(RequestInterface $request, ResponseInterface $response)
     {
+        $countAdd = 0;
+        $countSkip = 0;
         $orders = $this->soap->getCallOrders([
             "auth" => [
                 "login" => $this->container->get("settings")["soap"]["queryLogin"],
                 "password" => $this->container->get("settings")["soap"]["queryPassword"]
             ]
+        ])->orderInfo;
+
+        foreach ($orders as $item) {
+
+            if (Order::where('topdelivery_id', $item->orderIdentity->orderId)->count() === 0) {
+                $countAdd++;
+                $order = new Order();
+
+                $order->topdelivery_id = $item->orderIdentity->orderId;
+                $order->data = json_encode($item);
+                $order->status = 0;
+                $order->member_id = null;
+
+                $order->save();
+
+            } else {
+                $countSkip++;
+            };
+
+        }
+
+        return $response->withJson([
+            "message" => "Добавленно " . $countAdd . " записей. Пропущенно " . $countSkip . " записей."
         ]);
 
-        var_dump($orders);
+
     }
 
 }
